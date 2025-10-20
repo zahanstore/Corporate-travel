80fa9dfad54a53c25b58d368592fa89d103afcfa
zahan <storezahan@gmail.com>
2025-10-20 19:49:42 +0530

    Create hello.js

---
+++ functions/hello.js
@@ -0,0 +1,10 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ 
+      message: "Hello from Netlify Functions!",
+      working: true,
+      timestamp: new Date().toISOString()
+    })
+  };
+};
